ea836ba53bb0e0686d1ec0d3d61534b66283e15c
18252034271 <wangxu0>
2020-09-10 17:19:39 +0800

    second commit

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -3,5 +3,6 @@ package helloworld;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("这里是我第一个要上传到git的项目");
+		System.out.println("第二次提交2020.09.10 17:19:11");
 	}
 }
